f3832ba2ab366c58bd40a3ec359abda6c01a7c97
xXDeonoXx <romullocordeiro@gmail.com>
2019-02-22 21:23:32 -0300

    comentario

--- src/main/java/com/romullocordeiro/imageprovider/ImageProviderApplication.java
+++ src/main/java/com/romullocordeiro/imageprovider/ImageProviderApplication.java
@@ -2,12 +2,14 @@ package com.romullocordeiro.imageprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ImageProviderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImageProviderApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("5Urr0g4t35"));
 	}
 
 }

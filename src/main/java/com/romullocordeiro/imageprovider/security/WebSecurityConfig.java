c283bd17b553f8c5a98b2b5e087d2a7e9cb70a37
xXDeonoXx <romullocordeiro@gmail.com>
2019-02-26 10:50:40 -0300

    comment

--- src/main/java/com/romullocordeiro/imageprovider/security/WebSecurityConfig.java
+++ src/main/java/com/romullocordeiro/imageprovider/security/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception{
 		http.csrf().disable().authorizeRequests()
 		.antMatchers(HttpMethod.GET, "/api/imagens").permitAll()
-		.antMatchers(HttpMethod.GET, "/api/imagens/tag").permitAll()
+		.antMatchers(HttpMethod.GET, "/api/imagens/tag/").permitAll()
 		.anyRequest().authenticated()
 		.and().formLogin().permitAll()
 		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));

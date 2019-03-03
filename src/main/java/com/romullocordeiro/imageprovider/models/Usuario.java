86a7671c198075f1a7922270ab90c01b35635435
xXDeonoXx <romullocordeiro@gmail.com>
2019-03-03 16:28:06 -0300

    comment

--- src/main/java/com/romullocordeiro/imageprovider/models/Usuario.java
+++ src/main/java/com/romullocordeiro/imageprovider/models/Usuario.java
@@ -70,7 +70,7 @@ public class Usuario implements UserDetails{
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
-		return null;
+		return (Collection<? extends GrantedAuthority>) this.roles;
 	}
 
 	@Override
